e1fd8931f393164a864b9ae5a96db573575a61fb
Naimul Hoque <naimul@ucsb.edu>
2020-08-24 16:16:02 -0700

    Created a basic class definition for InventoryAllocator. Subject to change.

---
+++ src/InventoryAllocator.py
@@ -0,0 +1,13 @@
+# Naimul Hoque
+
+
+# Develop Inventory Allocator Class
+class InventoryAllocator:
+    def __init__(self):
+        self.department = department # String holding the specific department
+        self.item = [] # List of Strings representing the items in inventory
+        self.itemAmount = [] # List of integers representing the amount of a specific item. 
+
+
+# Main Program
+# User Input
