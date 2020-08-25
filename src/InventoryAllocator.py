6aa855876a0d3d01fd8ad070df44910d34ddb02a
Naimul Hoque <naimul@ucsb.edu>
2020-08-24 23:36:32 -0700

    Created two function: obtainStringsInput and decodeFirstString. obtainStringsInput will seperate input to two different strings making it easier to extract data. decodeFirstString extracts the item name and total number of that specific item ordered.

--- src/InventoryAllocator.py
+++ src/InventoryAllocator.py
@@ -8,28 +8,52 @@ class InventoryAllocator:
         self.item = [] # List of Strings representing the items in inventory
         self.itemAmount = [] # List of integers representing the amount of a specific item. 
 
-# Main Program
 
+# Since the user will enter two different inputs, it would be easy to work with data by seperating into two different strings. This function fulfills this purpose.
+def obtainStringsInput(userInput):
+    # First, find the indexes necessary to split the two inputs.
+    firstStart = userInput.find("{")
+    firstStop = userInput.find("}")
+    secondStart = userInput.find("[")
+    secondStop = userInput.find("]")
+
+    # Second, split the inputs to two different strings
+    firstInput = (userInput[firstStart+1:firstStop+1]).replace(" ","")
+    secondInput = (userInput[secondStart:secondStop+1]).replace(" ", "")
+
+    return firstInput,secondInput # Return the seperated strings
+
+def decodeFirstString(firstInput):
+    # Iterate through each character in the string.
+    item = [] # Holds the item that is being ordered
+    value = [] # Holds the total number of the specific item being ordered
+
+    # Using the sliding window method to extract the data from the string
+    initialIndex = 0 
+    currentIndex = 0
+    while(firstInput[currentIndex] != '}'): # If a closing brace occurs, this indicates end of first string
+        if(firstInput[currentIndex] == ':'): # If a colon occurs, indicates end of item name and beginning of total number of ordered item
+            item.append(firstInput[initialIndex:currentIndex]) 
+            initialIndex = currentIndex + 1 # We want initialIndex to start after colon
+            currentIndex += 1 # We want currentIndex to start after colon
+        elif(firstInput[currentIndex] == ","): # If a comma occurs, this indicates that there are more items being ordered.
+            value.append(firstInput[initialIndex:currentIndex])
+            initialIndex = currentIndex + 1 # We want initialIndex to start after comma
+            currentIndex += 1 # We want currentIndex to start after comma
+        else:
+            currentIndex += 1 # If none of the above occurs, increase currentIndex by 1
+
+    value.append(firstInput[initialIndex:currentIndex]) # This ensures last value is obtained since the loop is exited after a closing brace
+    return item,value
+
+# Main Program
 # Get user input. Necessary for testing purposes.
 # { apple: 1 }, [{ name: owd, inventory: { apple: 1 } }]
 userInput = input("Enter shipment information: ") # Gathers user input
-
-# After recieving input, we need to be able to break it down to apply the information to the program
-
-# First, find the indexes necessary to split the two inputs.
-firstStart = userInput.find("{")
-firstStop = userInput.find("}")
-secondStart = userInput.find("[")
-secondStop = userInput.find("]")
-
-# Second, split the inputs to two different strings
-firstInput = (userInput[firstStart:firstStop+1]).replace(" ","")
-secondInput = (userInput[secondStart:secondStop+1]).replace(" ", "")
-
-# Third, since we have seperated input to two strings, we can handle each input seperatly.
-# It would be best to handle the second string which holds information regarding warehouse name and inventory amounts
-print(firstInput)
-
+firstInput, secondInput = obtainStringsInput(userInput) # Calls function which returns both inputs seperated into two strings
+itemNames, itemTotal = decodeFirstString(firstInput) # Calls function which returns a list of item being ordered and total number of the specific item that was ordered
+print(itemNames)
+print(itemTotal)
 
 
 
