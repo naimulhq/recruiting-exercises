d954438ac06d9489de077f7861d723d8f9658b93
Naimul Hoque <naimul@ucsb.edu>
2020-08-24 19:57:07 -0700

    Began creating an input handler to extract content within input for future calculations.

--- src/InventoryAllocator.py
+++ src/InventoryAllocator.py
@@ -8,6 +8,31 @@ class InventoryAllocator:
         self.item = [] # List of Strings representing the items in inventory
         self.itemAmount = [] # List of integers representing the amount of a specific item. 
 
-
 # Main Program
-# User Input
+
+# Get user input. Necessary for testing purposes.
+# { apple: 1 }, [{ name: owd, inventory: { apple: 1 } }]
+userInput = input("Enter shipment information: ") # Gathers user input
+
+# After recieving input, we need to be able to break it down to apply the information to the program
+
+# First, find the indexes necessary to split the two inputs.
+firstStart = userInput.find("{")
+firstStop = userInput.find("}")
+secondStart = userInput.find("[")
+secondStop = userInput.find("]")
+
+# Second, split the inputs to two different strings
+firstInput = (userInput[firstStart:firstStop+1]).replace(" ","")
+secondInput = (userInput[secondStart:secondStop+1]).replace(" ", "")
+
+# Third, since we have seperated input to two strings, we can handle each input seperatly.
+# It would be best to handle the second string which holds information regarding warehouse name and inventory amounts
+print(firstInput)
+
+
+
+
+
+
+
