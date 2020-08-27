83d763b6f08073c889b3708af57d569bb396ffef
Naimul Hoque <naimul@ucsb.edu>
2020-08-27 03:43:26 -0700

    Added second test case from README. All test passed!

--- src/testInventoryAllocator.py
+++ src/testInventoryAllocator.py
@@ -5,6 +5,9 @@ class testInventoryAllocator(unittest.TestCase):
     def test_perfectCaseOneItemOneWarehouse(self):
         result = main("{ apple: 1 }, [{ name: owd, inventory: { apple: 1 } }]")
         self.assertEqual(result, "{ owd: { apple: 1 } }" )
+    def test_oneWarehouseNotEnoughInventory(self):
+        result = main("{ apple: 1 }, [{ name: owd, inventory: { apple: 0 } }]")
+        self.assertEqual(result,"[]")
 
 if __name__ == '__main__':
     unittest.main()
