0c89b8795beceece790a9cbacabc7a1c8c406131
Naimul Hoque <naimul@ucsb.edu>
2020-08-27 04:02:03 -0700

    Added third test case. Has not been successful yet!

--- src/testInventoryAllocator.py
+++ src/testInventoryAllocator.py
@@ -3,11 +3,17 @@ import os
 from InventoryAllocator import main
 class testInventoryAllocator(unittest.TestCase):
     def test_perfectCaseOneItemOneWarehouse(self):
+        print("Test 1")
         result = main("{ apple: 1 }, [{ name: owd, inventory: { apple: 1 } }]")
         self.assertEqual(result, "{ owd: { apple: 1 } }" )
     def test_oneWarehouseNotEnoughInventory(self):
+        print("Test 2")
         result = main("{ apple: 1 }, [{ name: owd, inventory: { apple: 0 } }]")
         self.assertEqual(result,"[]")
+    def test_oneItemTwoWarehouseSplit(self):
+        print("Test 3")
+        result = main("{ apple: 10 }, [{ name: owd, inventory: { apple: 5 } }, { name: dm, inventory: { apple: 5 }}]")
+        self.assertEqual(result,"[{ dm: { apple: 5 }}, { owd: { apple: 5 } }]")
 
 if __name__ == '__main__':
     unittest.main()
