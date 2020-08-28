f284133ca40e3d1ae7634150cac349046da33035
Naimul Hoque <naimul@ucsb.edu>
2020-08-27 17:56:07 -0700

    Added new test. Test to see two items in one order is satisfied for one warehouse. Currenty 3/4 in tests passed!

--- src/testInventoryAllocator.py
+++ src/testInventoryAllocator.py
@@ -14,6 +14,11 @@ class testInventoryAllocator(unittest.TestCase):
         print("Test 3")
         result = main("{ apple: 10 }, [{ name: owd, inventory: { apple: 5 } }, { name: dm, inventory: { apple: 5 }}]")
         self.assertEqual(result,"[{ dm: { apple: 5 }}, { owd: { apple: 5 } }]")
+    def test_perfectTwoItemOneWarehouse(self):
+        print("Test 4")
+        result = main("{ apple: 1, banana: 2 }, [{ name: owd, inventory: { apple: 1, banana: 2 } }]")
+        self.assertEqual(result,"{ owd: { apple: 1, banana: 2 } }")
+
 
 if __name__ == '__main__':
     unittest.main()
