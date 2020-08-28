da36d6abfab97cad232d41ea3aa56230c272ac05
Naimul Hoque <naimul@ucsb.edu>
2020-08-27 19:22:24 -0700

    Added two new tests: One test checks two warehouses when item is not in any, the other fulfills order of two item, where each item is in specific warehouse

--- src/testInventoryAllocator.py
+++ src/testInventoryAllocator.py
@@ -22,7 +22,17 @@ class testInventoryAllocator(unittest.TestCase):
         print("Test 5")
         result = main("{ apple: 2 }, [{ name: owd, inventory: { apple: 2 } }, { name: dm, inventory: { banana: 3 }}]")
         self.assertEqual(result,"{ owd: { apple: 2 } }" )
-
-
+    def test_oneItemTwoWarehouse2(self):
+        print("Test 6")
+        result = main("{ banana: 3 }, [{ name: owd, inventory: { apple: 2 } }, { name: dm, inventory: { banana: 3 }}]")
+        self.assertEqual(result,"{ dm: { banana: 3 } }" )
+    def test_oneItemNotInEitherWarehouse(self):
+        print("Test 7")
+        result = main("{ orange: 3 }, [{ name: owd, inventory: { apple: 2 } }, { name: dm, inventory: { banana: 3 }}]")
+        self.assertEqual(result,"[]")
+    def test_twoItemOneEachWarehouse(self):
+        print("Test 8")
+        result = main("{ apple: 1, banana: 2 }, [{ name: owd, inventory: { apple: 1 } }, { name: dm, inventory: { banana: 2 } }]")
+        self.assertEqual(result,"{ owd: { apple: 1 } }, { dm: { banana: 2 } }")
 if __name__ == '__main__':
     unittest.main()
